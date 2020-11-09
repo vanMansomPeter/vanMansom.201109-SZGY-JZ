9ca926d67b4ea18e81d2c01b7cd17a91b2a943df
kecskegamer2001 <70634456+kecskegamer2001@users.noreply.github.com>
2020-11-09 10:01:12 +0100

    Add project files.

---
+++ vanMansom.201109 SZGY JZ/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vanMansom._201109_SZGY_JZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
