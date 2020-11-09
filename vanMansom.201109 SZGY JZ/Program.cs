b4bfa7915a41d6469a8d306e6c5b24eb235f26f7
kecskegamer2001 <70634456+kecskegamer2001@users.noreply.github.com>
2020-11-09 10:31:16 +0100

    dfhbd

--- vanMansom.201109 SZGY JZ/Program.cs
+++ vanMansom.201109 SZGY JZ/Program.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace vanMansom._201109_SZGY_JZ
 {
+    //érték típus struct
+    struct EmberS
+    {
+        public string nev;
+        public int kor;
+    }
+    //referencia típus class
+    class EmberC
+    {
+        public string nev;
+        public int kor;
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            var s1 = new EmberS() { nev = "Zoli", kor = 30 };
+            var c1 = new EmberC() { nev = "Zoli", kor = 30 };
+            //Deklarál egy új vátozót, majd másol minden értéket- érték típus      
+            var s2 = s1;
+            // létrehoz egy ALIAST, ami ugyan arra a memória területre mutat-referencia típus
+            var c2 = c1;
+            //Az új megvátozik, az eredeti marad
+            s2.nev = "Jolánka";
+            //mivel ez csak egy referencia, ezért akár c1-et vagy c2-őt kérdezném le ugyan az lenne az eredmény
+            c2.nev = "Jolánka";
+
+            Console.WriteLine($"snev: {s1.nev}\nskor: {s1.kor}");
+
+            Console.WriteLine($"cnev: {c1.nev}\nckor: {c1.kor}");
+
+            Console.ReadKey();
         }
     }
 }
